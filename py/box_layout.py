97a125d0471a6f23907a32fd1d478177985efeec
Rain <hxrain@users.noreply.github.com>
2021-03-13 10:35:01 +0800

    二分树box布局管理器优化重构完成.

--- py/box_layout.py
+++ py/box_layout.py
@@ -8,8 +8,40 @@
     'L'  # 左边box
     'R'  # 右边box
     分割动作类型:
-    'H'  # 水平线分割,得到上下两个box
-    'V'  # 垂直线分割,得到左右两个box
+    'TB'  # 水平线分割,得到上下两个box
+    'LR'  # 垂直线分割,得到左右两个box
+
+分割比例sp的设定,假设目标线段坐标为(a,b):
+    sp为float时,按目标距离的百分比分割;
+    sp为int且>=0时,按相对a的距离分割.
+    sp为int且<0时,按相对b的反向距离分割.
+
+#用法示例
+layout = box_layout(640, 480)       #生成布局器,告知初始宽高
+layout('LR', 0.5)                   #对根节点整体范围进行左右分割
+layout['L']('LR', 0.5)              #对根下左侧节点进行左右分割
+layout['R']('TB', 0.25)             #对根下右侧节点进行上下分割
+layout.print()                      #调试输出分割结果
+layout.refresh(1024, 768)           #刷新整体宽高定义
+layout.print()                      #调试输出分割结果
+assert(layout['L']['R']==layout['/L/R']) #两种方式对目标box进行选取
+
+''' 上述示例代码的输出:
+640x480|(0,0)-(640,480)|LR:0.5
+    320x480|(0,0)-(320,480)|LR:0.5
+        160x480|(0,0)-(160,480)
+        160x480|(160,0)-(320,480)
+    320x480|(320,0)-(640,480)|TB:0.25
+        320x120|(320,0)-(640,120)
+        320x360|(320,120)-(640,480)
+1024x768|(0,0)-(1024,768)|LR:0.5
+    512x768|(0,0)-(512,768)|LR:0.5
+        256x768|(0,0)-(256,768)
+        256x768|(256,0)-(512,768)
+    512x768|(512,0)-(1024,768)|TB:0.25
+        512x192|(512,0)-(1024,192)
+        512x576|(512,192)-(1024,768)
+'''
 
 """
 
@@ -18,13 +50,15 @@ def box_split(rect, op, sp):
     """对rect按op类型进行sp比例分割,得到两个新的rect"""
 
     def calc(a, b, sp):
-        """计算数轴a,b两点间的分割点,sp为float时,分割按百分比;sp为int时分割按相对a的距离."""
+        """计算数轴a,b两点间的分割点,sp为float时,分割按百分比;sp为int且>=0时分割按相对a的距离."""
         if isinstance(sp, float):
             return int(a + (b - a) * sp)
-        else:
+        elif sp >= 0:
             return min(a + sp, b)
+        else:
+            return min(b + sp, a)
 
-    if op == 'H':
+    if op == 'TB':
         np = calc(rect[1], rect[3], sp)  # 水平线分割时,需要对top,bottom进行sp分割计算
         rect_1 = [rect[0], rect[1], rect[2], np]
         rect_2 = [rect[0], np, rect[2], rect[3]]
@@ -76,26 +110,29 @@ class box:
 
     def __getitem__(self, item):
         """访问当前box的子box,根据子box的布局名字,以及实际的布局进行检查输出."""
-        if self.op == 'H':
-            if item == 'T':
-                return self.child1
-            elif item == 'B':
-                return self.child2
+        if len(item) == 1 and item in {'T', 'B', 'L', 'R'}:
+            if self.op == 'TB':
+                if item == 'T':
+                    return self.child1
+                elif item == 'B':
+                    return self.child2
+            else:
+                if item == 'L':
+                    return self.child1
+                elif item == 'R':
+                    return self.child2
+            return None
         else:
-            if item == 'L':
-                return self.child1
-            elif item == 'R':
-                return self.child2
-        return None
+            return self.query(item)
 
     def __str__(self):
         """调试方便,用于显示内部核心数据"""
         if self.rect is None:
             return 'empty'
         if self.op:
-            return '(%d,%d)-(%d,%d)|%dx%d|%s:%s' % (self.rect[0], self.rect[1], self.rect[2], self.rect[3], self.width(), self.height(), self.op, self.sp)
+            return '%dx%d|(%d,%d)-(%d,%d)|%s:%s' % (self.width(), self.height(), self.rect[0], self.rect[1], self.rect[2], self.rect[3], self.op, self.sp)
         else:
-            return '(%d,%d)-(%d,%d)|%dx%d' % (self.rect[0], self.rect[1], self.rect[2], self.rect[3], self.width(), self.height())
+            return '%dx%d|(%d,%d)-(%d,%d)' % (self.width(), self.height(), self.rect[0], self.rect[1], self.rect[2], self.rect[3])
 
     def __call__(self, *args, **kwargs):
         """对当前节点,按op类型,进行sp比例分割.返回值:当前节点,或None(已经分割过的节点不能再次分割)"""
@@ -105,7 +142,7 @@ class box:
         op = args[0]
         sp = args[1]
 
-        if op not in {'H', 'V'}:
+        if op not in {'TB', 'LR'}:
             return None
         if not isinstance(sp, float) and not isinstance(sp, int):
             return None
@@ -132,14 +169,6 @@ class box:
             node = node[p]
         return node
 
-
-class box_layout(box):
-    """box布局管理器,其自身就是根节点"""
-
-    def __init__(self, width, height):
-        super().__init__(self)
-        self.refresh(width, height)
-
     def refresh(self, width, height):
         """对布局树进行刷新计算,所有子节点的box范围进行更新."""
         self.rect = [0, 0, width, height]
@@ -156,3 +185,24 @@ class box_layout(box):
 
         # 从根节点进行深度递归
         recalc(self)
+
+    def print(self):
+        """调试输出布局树"""
+
+        def out(node, deep):
+            tab = 4 * deep * ' '
+            print('%s%s' % (tab, node))
+            if node.op is None:
+                return
+            out(node.child1, deep + 1)
+            out(node.child2, deep + 1)
+
+        out(self, 0)
+
+
+class box_layout(box):
+    """box布局管理器,其自身就是根节点"""
+
+    def __init__(self, width, height):
+        super().__init__(self)
+        self.refresh(width, height)
