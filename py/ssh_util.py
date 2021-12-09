22131a73595816308626241e1472e378640651f6
Rain <hxrain@users.noreply.github.com>
2021-12-09 14:59:34 +0800

    SSH客户端功能扩展.

--- py/ssh_util.py
+++ py/ssh_util.py
@@ -2,7 +2,7 @@ import paramiko
 from io import BytesIO
 
 
-class SSHClient:
+class SFTPClient:
 
     def __init__(self, ip=None, username='root', password='123456,', port=22):
         self.sftp = None
@@ -72,3 +72,105 @@ class SSHClient:
         if self.transport:
             self.transport.close()
             self.transport = None
+
+
+class ssh_take_t:
+    """进行文件下载"""
+
+    def __init__(self):
+        self.clts = {}
+
+    def take(self, host, rmtfile, user=None, pwd=None):
+        """下载指定服务器上的指定路径文件.返回值:(文件内容,错误消息),错误消息为空正常."""
+        if host not in self.clts:
+            self.clts[host] = SFTPClient(host)
+            if user or pwd:
+                self.clts[host].init(host, user, pwd)
+
+        clt = self.clts[host]
+        if not clt.opend():
+            msg = clt.open()
+            if msg:
+                return None, msg
+
+        dat, msg = clt.data_get(rmtfile, 'utf-8')
+        if msg:
+            clt.close()
+            return None, msg
+
+        return dat, ''
+
+
+class SSHClient:
+    """SSH客户端功能对象"""
+
+    def __init__(self, ip=None, username='root', password='123456,', port=22):
+        self.clts = {}
+        self.ssh = None
+        self.init(ip, username, password, port)
+
+    def init(self, ip, username='root', password='123456,', port=22):
+        self.ip = ip
+        self.port = port
+        self.username = username
+        self.password = password
+
+    def close(self):
+        if self.ssh:
+            self.ssh.close()
+            self.ssh = None
+
+    def open(self):
+        """打开sftp连接"""
+        self.close()
+        try:
+            self.ssh = paramiko.SSHClient()
+            self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            self.ssh.connect(hostname=str(self.ip), port=int(self.port), username=self.username, password=self.password)
+            return ''
+        except Exception as e:
+            self.close()
+            return str(e)
+
+    def opend(self):
+        return self.ssh is not None
+
+    def exec(self, cmd):
+        """运行指定服务器上的指定路径文件.返回值:(输出内容,错误消息),错误消息为空正常."""
+        dat = None
+        msg = ''
+        try:
+            si, so, se = self.ssh.exec_command(cmd)
+            dat = so.readlines()
+            msg = se.readlines()
+        except Exception as e:
+            return None, [str(e)]
+        return dat, msg
+
+
+class ssh_exec_t:
+    """在指定服务器上运行指定的命令"""
+
+    def __init__(self):
+        self.clts = {}
+
+    def exec(self, host, cmd, user=None, pwd=None):
+        """下载指定服务器上的指定路径文件.返回值:(文件内容,错误消息),错误消息为空正常."""
+        if host not in self.clts:
+            self.clts[host] = SSHClient(host)
+            if user or pwd:
+                self.clts[host].init(host, user, pwd)
+
+        clt = self.clts[host]
+        if not clt.opend():
+            msg = clt.open()
+            if msg:
+                return None, [msg]
+
+        dat, msg = clt.exec(cmd)
+        if dat is None:
+            clt.close()
+            return None, msg
+
+        return dat, msg
+
