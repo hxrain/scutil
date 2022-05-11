f9ed71107349fac602dc79d5c5a660652c0042eb
Rain <hxrain@users.noreply.github.com>
2022-05-11 13:43:50 +0800

    修正特殊.docx文件格式的识别.

--- py/spd_base.py
+++ py/spd_base.py
@@ -44,34 +44,43 @@ def magic_mime(data, to_extname=False):
 
 
 def guess_mime(data, to_extname=False):
-    """按data内容扩展分析猜测对应的mime类型;to_extname告知是否转换为文件扩展名."""
+    """按data内容扩展分析猜测对应的mime类型;to_extname告知是否转换为文件扩展名.
+       返回值:结果MIME类型串或文件扩展名
+    """
     # 调用magic库猜测数据的格式
     if isinstance(data, str):
         dat = data.encode('utf-8')
     else:
         dat = data
     rt = magic_mime(dat, to_extname)
-    if rt not in {'.txt', 'text/plain'}:
-        return rt  # 如果结果不是文本,则直接返回
+    if rt in {'.txt', 'text/plain'}:
+        # 文本类型,进行后续增强判断
+        try:
+            json.loads(data)  # 先尝试装载json格式
+            return '.json' if to_extname else 'application/json'
+        except:
+            pass
 
-    # 文本类型,进行后续增强判断
-    try:
-        json.loads(data)  # 先尝试装载json格式
-        return '.json' if to_extname else 'application/json'
-    except:
-        pass
+        try:
+            minidom.parseString(data)  # 尝试进行xml格式分析
+            return '.xml' if to_extname else 'application/xml'
+        except:
+            pass
 
-    try:
-        minidom.parseString(data)  # 尝试进行xml格式分析
-        return '.xml' if to_extname else 'application/xml'
-    except:
-        pass
+        try:
+            etree.HTML(data)  # 再尝试装载html格式
+            return '.html' if to_extname else 'text/html'
+        except:
+            pass
 
-    try:
-        etree.HTML(data)  # 再尝试装载html格式
-        return '.html' if to_extname else 'text/html'
-    except:
-        pass
+    if rt in {'.zip', 'application/zip'}:
+        # 对误识别的zip文件进行额外的特征校正
+        if dat.startswith(b'\x50\x4B\x03\x04\x0A\x00\x00\x00\x00\x00\x87\x4E\xE2\x40'):  # .docx
+            return '.docx' if to_extname else 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+        if dat.startswith(b'\x50\x4B\x03\x04\x0A\x00\x00\x00\x00\x00\x87\x4E\xE2\x40'):  # TODO .xlsx
+            return '.xlsx' if to_extname else 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        if dat.startswith(b'\x50\x4B\x03\x04\x0A\x00\x00\x00\x00\x00\x87\x4E\xE2\x40'):  # TODO .pptx
+            return '.pptx' if to_extname else 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
 
     return rt
 
