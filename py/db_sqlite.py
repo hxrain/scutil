161994750dfcdc6193c1f99acd11f8bc0f1762a8
Rain <hxrain@users.noreply.github.com>
2022-12-15 10:04:56 +0800

    补充了注释说明,移除无用代码.

--- py/db_sqlite.py
+++ py/db_sqlite.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
 
 import sqlite3 as s
-from hash_calc import *
 
 
-# sqlite3数据库功能封装
 class s3db:
+    """sqlite3数据库功能封装"""
+
     def __init__(self, dbpath=None):
+        """如果构造时给出了数据库路径,则直接打开"""
         self.conn = None
         if dbpath is not None:
             self.open(dbpath)
 
     def opened(self):
+        """判断是否打开了数据库"""
         return self.conn is not None
 
     def open(self, dbpath):
+        """打开指定的数据库,返回值:是否成功"""
         if self.conn is not None:
             return True
 
@@ -25,6 +28,7 @@ class s3db:
             return False
 
     def opt_set(self, cmd, val):
+        """设定sqlite3配置参数,返回值:是否成功"""
         if self.conn is None:
             return False
 
@@ -43,24 +47,32 @@ class s3db:
         self.opt_set('Cache_Size', '16384')
 
     def close(self):
+        """关闭数据库连接"""
         if self.conn is None:
             return True
         self.conn.close()
         self.conn = None
 
-    def exec(self, sql, w=None):
+    def exec(self, sql, data=None, commit=True):
+        """执行sql语句(不返回结果集的增删改语句).
+            data - sql的绑定参数
+            commit - 告知是否立即提交.不是立即提交时需要外部执行commit()方法手动提交,提高性能.
+            返回值: (是否成功,错误消息)
+        """
         try:
-            if w is None:
+            if data is None:
                 self.conn.execute(sql)
             else:
-                self.conn.execute(sql, w)
-            self.conn.commit()
+                self.conn.execute(sql, data)
+            if commit:
+                self.conn.commit()
             return True, ''
         except Exception as e:
             self.conn.rollback()
             return False, str(e)
 
     def commit(self):
+        """手动提交执行结果.返回值(是否成功,错误消息)"""
         try:
             self.conn.commit()
             return True, ''
@@ -79,22 +91,28 @@ for row in rows:
 '''
 
 
-# sqlite3数据查询功能封装
 class s3query:
+    """sqlite3数据查询功能封装"""
+
     def __init__(self, db):
+        """构造时绑定数据库对象"""
         self.db = db
         if db.conn:
             self.open(db)
 
     # 初始化查询对象,可指定数据库对象
     def open(self, db=None):
+        """创建查询游标对象."""
         if db is None:
             db = self.db
         self.conn = db.conn
         self.cur = db.conn.cursor()
 
     def exec(self, sql, param=None, cmt=True):
-        """执行sql语句,不要求获取结果集"""
+        """执行sql语句,不要求获取结果集.
+            返回值:成功时 - (True,影响的行数)
+                  失败时 - (False,错误信息)
+        """
         try:
             if param is None:
                 self.cur.execute(sql)
@@ -108,7 +126,10 @@ class s3query:
             return False, str(e)
 
     def query(self, sql, param=None, fetchsize=None):
-        """执行sql查询,得到结果集(默认是得到全部,也可以指定获取的数量)"""
+        """执行sql查询,得到结果集(默认是得到全部,也可以指定获取的数量)
+            返回值:成功时 - (结果集,'')
+                  失败时 - (None,错误信息)
+        """
         try:
             if param is None:
                 self.cur.execute(sql)
@@ -122,14 +143,20 @@ class s3query:
             return None, str(e)
 
     def fetch(self, fetchsize):
-        """在query之后,获取结果集的后续部分"""
+        """在query使用了fetchsize分批获取之后,继续获取结果集的后续部分
+            返回值:成功时 - (结果集,'')
+                  失败时 - (None,错误信息)
+        """
         try:
             return self.cur.fetchmany(size=fetchsize), ''
         except Exception as e:
             return None, str(e)
 
     def append(self, obj, cmt=True):
-        """轻量级ORM插入实现,obj的类型为表名,obj内含属性为表中字段与对应的值"""
+        """轻量级ORM插入实现,obj的类型为表名,obj内含属性为表中字段与对应的值
+            返回值:成功时 - (True,影响的行数)
+                  失败时 - (False,错误信息)
+        """
 
         def _insert(obj, cmt):
             tbl = type(obj).__name__
@@ -154,7 +181,9 @@ class s3query:
             return _insert(obj, cmt)
 
     def extract(self, sql, filter_fun, param=None, fetchsize=100):
-        """执行查询,给出sql和参数param,对结果行进行filter_fun过滤,可设定提取批尺寸fetchsize"""
+        """执行查询,给出sql和参数param,对结果行进行filter_fun过滤,可设定提取批尺寸fetchsize.
+           返回值:成功时 - (结果数量,错误信息)
+        """
         rc = 0
         rows, msg = self.query(sql, param, fetchsize)
         if msg: return rc, msg
@@ -167,13 +196,17 @@ class s3query:
         return rc, msg
 
     def has(self, name, type='table'):
-        """判断指定的库表对象table/index/view是否存在.返回值:None查询失败,结果未知;True/False告知是否存在"""
+        """判断指定的库表对象table/index/view是否存在.
+            返回值:None - 查询失败,结果未知
+                  True/False - 告知是否存在
+        """
         rows, msg = self.query("SELECT name FROM sqlite_master WHERE type=? and name=?", (type, name))
         if msg != '':
             return None
         return len(rows) > 0
 
     def close(self):
+        """关闭数据查询对象"""
         if self.cur is not None:
             self.cur.close()
         self.cur = None
@@ -190,108 +223,52 @@ class s3tbl(s3query):
         self.sql_select = None
 
     def set_insert(self, sql):
+        """绑定insert语句"""
         self.sql_insert = sql
 
     def set_update(self, sql):
+        """绑定update语句"""
         self.sql_update = sql
 
     def set_select(self, sql):
+        """绑定select语句"""
         self.sql_select = sql
 
     def insert(self, vals, cmt=True):
+        """插入记录vals
+            返回值:成功时 - (True,影响的行数)
+                  失败时 - (False,错误信息)
+        """
         return super().exec(self.sql_insert, vals, cmt)
 
     def insertx(self, lst):
+        """插入多条记录lst"""
         for t in lst:
             self.insert(t, False)
         self.conn.commit()
 
     def update(self, vals, where=None, cmt=True):
+        """更新记录
+            返回值:成功时 - (True,影响的行数)
+                  失败时 - (False,错误信息)
+        """
         if where is None:
             return super().exec(self.sql_update, vals, cmt)
         else:
             return super().exec(self.sql_update, vals + where, cmt)
 
     def query(self, param=None, sql=None):
+        """查询结果
+            返回值:成功时 - (结果集,'')
+                  失败时 - (None,错误信息)
+        """
         if sql is None:
             sql = self.sql_select
         return super().query(sql, param)
 
     def close(self):
+        """关闭"""
         super().close()
         self.sql_insert = None
         self.sql_update = None
         self.sql_select = None
-
-
-'''
-db=s3db("dat.s3db")
-db.opt_def()
-sw=s3_writer(db,(0,1))
-sw.open('select name,url from cops',"insert into cops(name,url) values(?,?)")
-sw.append(('n1','u1'))
-sw.append(('n2','u2'))
-'''
-
-
-class s3_writer:
-    def __init__(self, db, keyIdx=0):
-        self.tbl = s3tbl(db)
-        self.keys = None
-        self.keyIdx = keyIdx
-
-    def open(self, select_keys, sql_insert=None):
-        if self.keys is not None:
-            return True
-
-        if sql_insert is not None:
-            self.tbl.set_insert(sql_insert)
-        try:
-            self.keys = set()
-            for fields in self.tbl.query(None, select_keys):
-                ks = ''.join(fields)
-                self.keys.add(calc_key(ks))  # 记录当前行数据的唯一key,便于排重
-
-            return True
-        except Exception as e:
-            return False
-
-    def append(self, line, cmt=True):
-        """追加行内容到数据表.返回值:-1 DB未打开;-2其他错误;1内容重复;2正常完成."""
-        if self.keys is None:
-            return -1, ''
-
-        key = calc_key(line, self.keyIdx)
-        if key in self.keys:
-            return 1, ''
-        try:
-            self.tbl.insert(line, cmt)
-            self.keys.add(key)
-            return 2, ''
-        except Exception as e:
-            return -2, 'ERR: ' + str(e) + ' append' + str(line)
-
-    def appendx(self, lst):
-        """追加元组列表到文件"""
-        if self.keys is None:
-            return -1
-        for l in lst:
-            r, m = self.append(l, False)
-            if r < 0:
-                print(m)
-        self.tbl.conn.commit()
-        return 2
-
-    def save(self):
-        if self.fp is None:
-            return False
-        self.fp.flush()
-        return True
-
-    def close(self):
-        if self.fp is None:
-            return False
-        self.fp.close()
-        self.fp = None
-        self.keys = None
-        return True
