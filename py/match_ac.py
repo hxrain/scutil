730f11bba7379be969b5e70422e736c263c7ff45
Rain <hxrain@users.noreply.github.com>
2022-12-20 17:25:08 +0800

    初步完成了ac自动机的功能封装.

---
+++ py/match_ac.py
@@ -0,0 +1,263 @@
+# AC自动机 用于多模式匹配的应用
+from collections import deque
+from typing import List
+from enum import Enum, unique
+from match_util import rep_rec_t
+
+
+@unique
+class mode_t(Enum):
+    """匹配结果的记录模式"""
+    max_match = 0  # 后项最大化优先匹配
+    is_all = 1  # 全匹配模式
+
+
+class ac_match_t:
+    """AC自动机多模式匹配功能封装"""
+
+    delimit = '\x00'
+
+    class node_t:
+        def __init__(self) -> None:
+            self.char = None  # 当前节点字母
+            self.childs = {}  # 当前节点的子节点分支 {char:node_t}
+            self.words = None  # 当前节点前缀词的长度
+            self.end = None  # 当前节点是否是结束节点(不为None,就是替换目标值)
+            self.fail = None  # 当前节点所指向的fail指针
+            self.parent = None  # 当前节点的父节点
+
+        def pre_word(self):
+            """获取指定节点node的前缀词"""
+            rst = []
+            node = self
+            while node is not None and node.char is not None:
+                rst.insert(0, node.char)
+                node = node.parent
+            return ''.join(rst)
+
+        def get_fails(self):
+            """获取指定节点node的有效fail跳转路径"""
+            fails = []
+            node = self
+            if node.end is not None:
+                fails.append(node)
+            while node and node.fail:
+                if node.fail.end is not None:
+                    fails.insert(0, node.fail)
+                node = node.fail
+            return fails
+
+        def __repr__(self):
+            if self.parent is None and self.char is None:
+                return f'root({len(self.childs)})'
+            else:
+                return f'"{self.pre_word()}"={self.end}/{list(self.childs.keys())}@<{self.fail}>'
+
+    def __init__(self):
+        self.root = self.node_t()
+
+    def dict_add(self, keyword, val=delimit, strip=True):
+        """添加关键词条到词表
+            keyword - 待匹配的关键词
+            val - 匹配后对应的替换目标词
+            strip - 是否对关键词进行净空处理(可能会导致空格被丢弃)
+        """
+        word = keyword if not strip else keyword.strip()
+        node = pnode = self.root  # 从根进行节点树的遍历
+
+        for i, char in enumerate(word):  # 遍历关键词的每个字符,构建子节点树
+            if char not in node.childs:  # 如果当前层级的字符未出现在当前节点的下一级
+                node.childs[char] = self.node_t()  # 创建新的子节点
+                node = node.childs[char]  # 得到新的子节点
+                node.words = i + 1  # 当前节点前缀词的长度
+                node.char = char  # 记录节点对应的字符
+                node.parent = pnode  # 记录节点的父节点
+            else:
+                node = node.childs[char]  # 得到当前层级对应的子节点
+            pnode = node  # 准备进行下一级节点树的处理
+
+        node.end = val  # 最后一级的节点,记录替换值,表示一个单词的正向匹配树构建完成
+
+    def dict_end(self):
+        """在添加词表结束后,构建完整的fail跳转路径.
+            如果不进行fail路径的构建,则匹配行为就是简单的trie树(前项匹配)
+        """
+        root = self.root
+        queue = deque()  # 待处理节点队列,FIFO
+        for char in root.childs:  # 将根节点的全部子节点放入待处理队列
+            queue.append(root.childs[char])
+
+        # 从第一层开始,对trie树进行广度优先逐层遍历,由浅入深为每个节点增加Fail指针
+        while queue:
+            for i in range(len(queue)):
+                node = queue.popleft()  # 从待处理队列的前面取出当前待处理节点
+                for char in node.childs:  # 当前节点的全部子节点放入待处理队列的后边,确保完整的层级处理顺序
+                    queue.append(node.childs[char])
+
+                # 取出父节点的fail指针
+                pfail = node.parent.fail
+
+                # 向上寻找含有当前节点分支的上级fail节点
+                while pfail and node.char not in pfail.childs:
+                    pfail = pfail.fail
+
+                if pfail is None:  # 当前节点没有匹配的fail路径,直接指向根节点
+                    node.fail = root
+                else:  # 记录当前节点的匹配的fail指针(pfail的对应分支节点)
+                    node.fail = pfail.childs[node.char]
+
+    def dict_load(self, fname, isend=True, defval='\x00', sep='@', encoding='utf-8'):
+        """从文本文件fname装载词条.此函数可多次调用, 最后一次确保isend为真即可.
+            文件单行为一个词条配置,用sep分隔的左边值将被替换为右边值.没有sep分隔时,替换值为defval
+            返回值:(词条数量,错误消息)
+        """
+        try:
+            rc = 0
+            with open(fname, 'r', encoding='utf8') as f:
+                for line in f:
+                    line = line.strip()
+                    if not line:
+                        continue
+                    rc += 1
+                    dat = line.split(sep, 1)
+                    if len(dat) == 1:
+                        self.dict_add(dat[0], defval)
+                    else:
+                        self.dict_add(dat[0], dat[1])
+            if isend:
+                self.dict_end()
+            return rc, ''
+        except Exception as e:
+            return None, str(e)
+
+    def do_loop(self, cb, message, msg_len=None, offset=0):
+        """底层方法:对给定长度为msg_len的消息文本message从offset处进行循环匹配,将匹配结果回调反馈给cb(pos,node)"""
+        rc = 0  # 记录一共匹配了多少次
+        pos = offset
+        if msg_len is None:
+            msg_len = len(message)
+
+        def has_rst(node):
+            """判断当前node与其fail路径上,是否存在end端点"""
+
+        node = self.root  # 尝试从根节点进行初始匹配
+        while pos < msg_len:  # 在循环中,node就是状态机的当前状态
+            char = message[pos]
+            pos += 1
+            # 如果当前字符与当前状态节点不匹配,则跳转至状态节点的fail节点
+            while node and char not in node.childs:
+                node = node.fail
+
+            if node is None:  # 当前字符不存在匹配,则状态节点重新指向根节点,等待下一次初始匹配
+                node = self.root
+                continue
+
+            node = node.childs[char]  # 得到当前字符匹配的子节点
+            fails = node.get_fails()
+            if fails:
+                # 如果当前节点fail路径是存在的,则可能需要处理匹配结果
+                cb(pos, fails)
+                rc += 1
+
+        return rc
+
+    def do_check(self, message, msg_len=None, offset=0, mode: mode_t = mode_t.max_match):
+        """对给定的消息进行词条匹配测试,返回值:匹配结果[三元组(begin,end,val)]列表"""
+        rst = []
+
+        def cb_max(pos, fails):
+            """后项最大匹配,记录最后出现的有效结果"""
+            node = fails[-1]
+            if node.end is None:
+                return
+            b, e, v = pos - node.words, pos, node.end
+            while rst and b < rst[-1][1]:  # 新结果的起点小于已有结果的终点
+                rst.pop(-1)  # 踢掉旧结果
+            rst.append((b, e, v))
+
+        def cb_all(pos, fails):
+            """记录原文字符pos处匹配的节点node的全部可能值"""
+            for fail in fails:
+                rst.append((pos - fail.words, pos, fail.end))
+
+        # 根据要求的匹配模式,选取对应的结果记录函数
+        if mode == mode_t.max_match:
+            cb = cb_max
+        else:
+            cb = cb_all
+
+        # 执行匹配过程
+        self.do_loop(cb, message, msg_len, offset)
+        return rst
+
+    @staticmethod
+    def do_complete(matchs, message, msg_len=None):
+        """将匹配结果进行补全,得到完整的分段列表.其中(b,e,None)为原始文本段,(b,e,val)为匹配目标段"""
+        rst = []
+        if msg_len is None:
+            msg_len = len(message)
+        if not matchs:
+            return [(0, msg_len, None)]
+
+        pos = 0
+        for m in matchs:
+            if pos < m[0]:
+                rst.append((pos, m[0], None))
+                pos = m[1]
+            rst.append(m)
+
+        last = rst[-1]
+        if last[1] != msg_len:
+            rst.append((last[1], msg_len, None))
+        return rst
+
+    def do_match(self, message, msg_len=None, mode=mode_t.max_match):
+        """对给定的消息进行关键词匹配,得到补全过的结果链[(begin,end,val),(begin,end,val),...],val为None说明是原内容部分"""
+        if msg_len is None:
+            msg_len = len(message)
+
+        rs = self.do_check(message, msg_len, 0, mode)
+        if len(rs) == 0:
+            return []
+        return self.do_complete(rs, message, msg_len)
+
+    def do_filter(self, message, repl="*", msg_len=None, offset=0, rep_rec=None):
+        """对给定的消息进行匹配替换;没有替换词的时候使用repl代替;rep_rec可用来记录具体的替换过程.
+            返回值:替换后的文本内容
+        """
+        if msg_len is None:
+            msg_len = len(message)
+        ck_rst = self.do_check(message, msg_len, offset, mode_t.max_match)
+        if not ck_rst:
+            return message
+        cm_rst = self.do_complete(ck_rst, message, msg_len)
+
+        rst = []
+
+        def do_rep(begin, end, txt):
+            """记录替换结果,并进行替换信息的扩展记录"""
+            rst.append(txt)
+            if not rep_rec or not rep_rec.rst:
+                return
+            if self.rep_rec.txt is None:
+                self.rep_rec.txt = message
+            self.rep_rec.make(begin, end, txt)
+
+        for m in cm_rst:
+            if m[2] is None:
+                rst.append(message[m[0]:m[1]])
+                continue
+
+            if m[2] == self.delimit:
+                do_rep(m[0], m[1], repl * (m[1] - m[0]))
+                continue
+
+            dst_len = len(m[2])  # 替换的目标长度
+            mt_len = len(message[m[0]:m[1]])  # 匹配的内容长度
+            old_val = message[m[0]:m[0] + dst_len]  # 目标长度对应的内容
+            if dst_len > mt_len and max_match and old_val == m[2]:
+                rst.append(message[m[0]:m[1]])  # 替换的目标与原有值相同,不重复替换
+            else:
+                do_rep(m[0], m[1], m[2])
+
+        return ''.join(rst)
--- py/match_dfa.py
+++ py/match_dfa.py
@@ -1,73 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-class pos_t:
-    """匹配位置信息记录"""
-
-    def __init__(self, begin, end, txt, txtl=None, txtr=None):
-        self.begin = begin  # 开始位置[
-        self.end = end  # 结束位置)
-        self.txt = txt  # 开始结束对应的文本
-        self.txt_left = txtl  # 开始位置左侧的文本
-        self.txt_right = txtr  # 结束位置右侧的文本
-
-
-class rep_t:
-    """文本替换信息记录"""
-
-    def __init__(self):
-        self.src = None  # 匹配的源信息
-        self.dst = None  # 替换后的信息
-        self.tag = None  # 可选的标记
-
-    def rec_src(self, begin, end, txt, txtl='', txtr=''):
-        self.src = pos_t(begin, end, txt, txtl, txtr)
-
-    def rec_dst(self, begin, end, txt):
-        self.dst = pos_t(begin, end, txt)
-
-    def __repr__(self):
-        if self.src is None or self.dst is None:
-            return 'EMPTY'
-        return f"[{self.src.begin}:{self.src.end}='{self.src.txt}']=>[{self.dst.begin}:{self.dst.end}='{self.dst.txt}']"
-
-
-class rep_rec_t:
-    """文本替换信息的持续记录器"""
-
-    def __init__(self, txt=None, rst=None, lr_len=3):
-        self.adj = 0  # 目标偏移增量
-        self.txt = txt  # 原始文本串
-        self.lr_len = lr_len  # 额外记录的原始串左右内容长度
-        self.rst = rst
-
-    def make(self, begin, end, dst_txt, tag=None):
-        """生成替换记录对象.将[begin,end)的内容替换为dst_txt"""
-        if begin > end:
-            return None
-        ri = rep_t()
-        ri.tag = tag
-
-        # 匹配位置的左右内容
-        txtl = self.txt[0:begin] if begin <= self.lr_len else self.txt[begin - self.lr_len:begin]
-        txtr = self.txt[end: end + self.lr_len]
-        # 记录匹配信息
-        ri.rec_src(begin, end, self.txt[begin:end], txtl, txtr)
-
-        # 计算替换后的位置信息,需要持续考虑"替换目标的长度增量"
-        dst_begin = begin + self.adj
-        dst_end = dst_begin + len(dst_txt)
-        ri.rec_dst(dst_begin, dst_end, dst_txt)
-
-        # 迭代更新结果文本中的长度增量
-        self.adj += len(dst_txt) - (end - begin)
-        if self.rst is not None:
-            if len(self.rst) > 0:
-                assert (ri.src.begin >= self.rst[-1].src.end)
-                assert (ri.dst.begin >= self.rst[-1].dst.end)
-            self.rst.append(ri)
-        return ri
-
+from match_util import *
 
 # DFA前向最大匹配算法
 class dfa_match_t():
@@ -303,63 +236,6 @@ def do_filter(dfa, message, reps=None, repl="*", max_match=True, isall=False, lr
     dfa.rep_rec = rep_rec
     return dfa.do_filter(message, repl, max_match, isall), reps
 
-
-def lookup(reps, begin, end=None, by_src=True):
-    """在reps替换记录列表中,查找与[begin,end)相交的部分.返回值:[idx]列表"""
-    siz = len(reps)
-    if siz == 0:
-        return []
-
-    def val(m, is_begin=True):
-        """获取列表中m处元素begin或end属性的值."""
-        if by_src:
-            return reps[m].src.begin if is_begin else reps[m].src.end
-        else:
-            return reps[m].dst.begin if is_begin else reps[m].dst.end
-
-    if end == -1:
-        end = val(-1, False)
-    if end is None:
-        end = begin
-    if begin > end:
-        return []
-
-    lo = 0
-    hi = siz
-    pos = None
-    # 先用二分法定位begin最初的位置
-    while lo < hi:
-        mid = (lo + hi) // 2
-        vb = val(mid, True)
-        ve = val(mid, False)
-        if begin >= vb and begin < ve:
-            pos = mid
-            break
-        elif begin < vb:
-            hi = mid
-        else:
-            lo = mid + 1
-
-    if pos is None:
-        if lo >= siz:
-            return []
-        ve = val(lo, False)
-        if ve < begin:
-            return []
-        else:
-            pos = lo
-
-    # 从初始位置向后查找
-    rst = []
-    for i in range(pos, siz):
-        vb = val(i, True)
-        if vb >= end:
-            if begin >= vb:
-                rst.append(i)
-            break
-        rst.append(i)
-
-    return rst
 
 
 if __name__ == '__main__':
