a2717da3de788f6d6914ff4fd55af95a9eea244c
Rain <_rain_@126.com>
2026-04-07 12:15:23 +0800

    小调整.

--- py/crypto_aes.py
+++ py/crypto_aes.py
@@ -13,28 +13,26 @@ class AESCrypt:
         self.CRType = AES  # 记录加密器类型
 
         if mode == 'ecb':
-            emode = AES.MODE_ECB
+            self.emode = AES.MODE_ECB
         elif mode == 'cbc':
-            emode = AES.MODE_CBC
+            self.emode = AES.MODE_CBC
         else:
-            emode = AES.MODE_CBC
-
-        if iv is None:  # 根据是否有初始化向量决定如何构造加密器对象
-            self.cryptor = self.CRType.new(key_bytes, emode)
-        else:
-            self.cryptor = self.CRType.new(key_bytes, emode, iv)
-
+            self.emode = AES.MODE_CBC
+        self.iv = iv
+        self.key_bytes = key_bytes
         # 根据块尺寸构造对应的填充器
         self._padding = padding(self.cryptor.block_size, padmode)
 
     def encrypt(self, bytes):
         """加密指定的字节数据"""
         pln = self._padding.pad(bytes)
-        return self.cryptor.encrypt(pln)
+        cryptor = self.CRType.new(self.key_bytes, self.emode, self.iv)
+        return cryptor.encrypt(pln)
 
     def decrypt(self, bytes):
         """解密指定的字节数据"""
-        pln = self.cryptor.decrypt(bytes)
+        cryptor = self.CRType.new(self.key_bytes, self.emode, self.iv)
+        pln = cryptor.decrypt(bytes)
         return self._padding.unpad(pln)
 
 
@@ -43,9 +41,9 @@ def aes_base64_decode(dat, key, iv=None, pad='zero', mode='cbc'):
         key = key.encode('latin-1')
         if iv:
             iv = iv.encode('latin-1')
-        des = AESCrypt(key, mode=mode, padmode=pad, iv=iv)
+        aes = AESCrypt(key, mode=mode, padmode=pad, iv=iv)
         chp = base64.b64decode(dat)
-        edat = des.decrypt(chp)
+        edat = aes.decrypt(chp)
         return edat.decode('utf-8'), ''
     except Exception as e:
         return '', str(e)
